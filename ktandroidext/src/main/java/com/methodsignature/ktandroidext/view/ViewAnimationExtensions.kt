9f508e18713ba2dc03ef42c861fe18e980f3ec20
Randall Mitchell <randallmmitchell@gmail.com>
2018-03-27 22:24:27 -0400

    4 : Updates view fade utils to support erroneous requests.

--- ktandroidext/src/main/java/com/methodsignature/ktandroidext/view/ViewAnimationExtensions.kt
+++ ktandroidext/src/main/java/com/methodsignature/ktandroidext/view/ViewAnimationExtensions.kt
@@ -4,34 +4,54 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.view.View
 
-fun View.hideWithFade(onCompleteListener: (() -> Unit)? = null) {
-    animate().apply {
-        alpha(0f)
-        duration = 300
-        setListener(
-            object: AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator?) {
-                    visibility = View.GONE
-                    onCompleteListener?.invoke()
+/**
+ * Hides a view with a brief fade animation followed by a call to set the view to [View.GONE].
+ *
+ * If the view is already not [View.VISIBLE], the view will not be updated and the listener,
+ * if not null, will be invoked.
+ */
+fun View.fadeToGone(onCompleteListener: (() -> Unit)? = null) {
+    if (visibility != View.VISIBLE) {
+        onCompleteListener?.invoke()
+    } else {
+        animate().apply {
+            alpha(0f)
+            duration = 300
+            setListener(
+                object: AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        visibility = View.GONE
+                        onCompleteListener?.invoke()
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }
 
-fun View.showWithFade(onCompleteListener: (() -> Unit)? = null) {
-    alpha = 0f
-    visibility = View.VISIBLE
-    animate().apply {
-        alpha(1f)
-        duration = 300
-        setListener(
-            object: AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator?) {
-                    super.onAnimationEnd(animation)
-                    onCompleteListener?.invoke()
+/**
+ * Shows a view with a brief fade animation.
+ *
+ * If the view is already [View.VISIBLE], the view will not be updated and the listener,
+ * if not null, will be invoked.
+ */
+fun View.fadeToVisible(onCompleteListener: (() -> Unit)? = null) {
+    if (visibility == View.VISIBLE) {
+        onCompleteListener?.invoke()
+    } else {
+        alpha = 0f
+        visibility = View.VISIBLE
+        animate().apply {
+            alpha(1f)
+            duration = 300
+            setListener(
+                object: AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        onCompleteListener?.invoke()
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }
